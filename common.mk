OnePlus-Pad-2-development/android_device_oneplus_sm8650-common

sm8650-common: Initial PixelOS bringup
Signed-off-by: inferno0230 <mail@inferno0230.in>

common.mk
@@ -272,8 +272,13 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+ifeq ($(TARGET_IS_TABLET),true)
+    DEVICE_PACKAGE_OVERLAYS += \
+        $(LOCAL_PATH)/overlay-aosp-tablet
+else
+    DEVICE_PACKAGE_OVERLAYS += \
+        $(LOCAL_PATH)/overlay-aosp
+endif
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \

PixelOS-Devices/device_oneplus_sm8650-common

common.mk
@@ -272,8 +272,14 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+ifneq ($(TARGET_IS_TABLET),true)
+    DEVICE_PACKAGE_OVERLAYS += \
+        $(LOCAL_PATH)/overlay-aosp
+else
+    DEVICE_PACKAGE_OVERLAYS += \
+        $(LOCAL_PATH)/overlay-aosp-tablet
+
+endif
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \